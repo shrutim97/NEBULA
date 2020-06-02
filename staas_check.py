2d993e21dda72eb12c549dfa3320661f7761306f
Shruti Menon <46673978+shrutim97@users.noreply.github.com>
2020-06-02 22:46:19 +0530

    Update staas_check.py

--- staas_check.py
+++ staas_check.py
@@ -7,7 +7,7 @@ import subprocess
 import os
 import cgi
 
-
+#checking if storage has been created already for a particular student
 form=cgi.FieldStorage()
 usn=form.getvalue('usn')
 
@@ -15,7 +15,7 @@ present=subprocess.getoutput("ls /var/www/html/STAAS_CLOUD | grep '"+usn+"'")
 
 if (present == usn):
     print("content-type:text/html")
-    print("Location: http://192.168.43.72/STAAS_CLOUD/{0}".format(present))
+    print("Location: http://192.168.43.72/STAAS_CLOUD/{0}".format(present))                     #if storage present,show the directory listing ofthe storage
     print("\n")
 
     print('<html>')
@@ -26,7 +26,7 @@ if (present == usn):
 
 else:
     print("content-type:text/html")
-    print("Location: http://192.168.43.72/nodir.html?usn=%s" %usn )
+    print("Location: http://192.168.43.72/nodir.html?usn=%s" %usn )                             #else request for storage creation
     print("\n")
 
     print('<html>')
